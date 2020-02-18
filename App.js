f3a506c020a05fb3381caaaf73e42b3f0a16db02
Yu Okawara <solbonta@hotmail.com>
2020-02-18 22:15:06 +0900

    React練習 Navigater導入 20200218

--- App.js
+++ App.js
@@ -1,33 +1,15 @@
-// 共用のAppBarはこちらに残しておく
-
+// StackNavigater導入
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
-import AppBar from './src/components/AppBar.js'
 import MemoListScreen from './src/screens/MemoListScreen.js'
-import MemoDetailScreen from './src/screens/MemoDetailScreen.js';
-import MemoEditScreen from './src/screens/MemoEditScreen.js';
-import LoginScreen from './src/screens/LoginScreen.js';
-import SignupScreen from './src/screens/SignupScreen.js';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-          <AppBar />
-          <LoginScreen />
-      </View>
-    );
-  }
-}
-
-// Style //
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FAEBD7',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    paddingTop: 100,
+// StackNavigater
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: MemoListScreen
+    
   },
 });
+
+export default createAppContainer(AppNavigator);
